5f9f3d25ba8812975f17f724f648c070e8cc788d
Wendesday36 <bertazoeanna@gmail.com>
2023-11-14 15:46:58 +0100

    kesz

---
+++ osszetett/app/Http/Controllers/BasketController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Basket;
+use Illuminate\Http\Request;
+
+class BasketController extends Controller
+{
+    public function index(){
+        return Basket:: all();
+    }
+    public function show($id){
+        return Basket:: find($id);
+    }
+    public function destroy($id){
+        return Basket:: find($id)->delete();
+       // return redirect('/book/list');
+    }
+}
---
+++ osszetett/database/factories/Product_typeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product_type>
+ */
+class Product_typeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->sentence(),
+            'cost'=> rand(0,100)
+        ];
+    }
+}
